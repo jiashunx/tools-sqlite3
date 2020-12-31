8b419d68b6d37bd4949c865248bd043596fcb9e3
jiashunx <wenjiashun@163.com>
2020-12-31 19:17:56 +0800

    remove unite test code.

--- src/test/java/io/github/jiashunx/tools/sqlite3/SQLite3Test.java
+++ src/test/java/io/github/jiashunx/tools/sqlite3/SQLite3Test.java
@@ -1,16 +1,12 @@
 package io.github.jiashunx.tools.sqlite3;
 
 import io.github.jiashunx.tools.sqlite3.connection.SQLite3ConnectionManager;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-
-import static org.junit.Assert.*;
 
 /**
  * @author jiashunx
@@ -94,46 +90,6 @@ public class SQLite3Test {
         for (Thread thread: threadList) {
             thread.join();
         }
-    }
-
-    @Test
-    public void test() throws InterruptedException {
-        SQLite3JdbcTemplate jdbcTemplate = new SQLite3JdbcTemplate("test-sqlite.db");
-        SQLite3JdbcTemplate jdbcTemplate1 = new SQLite3JdbcTemplate(SQLite3ConnectionManager.getConnectionPool("test-sqlite.db"));
-        assertEquals(jdbcTemplate.getConnectionPool(), jdbcTemplate1.getConnectionPool());
-
-        boolean tableExists = jdbcTemplate.isTableExists("TEST_LEE");
-        System.out.println("table TEST_LEE exists ? " + tableExists);
-        if (!tableExists) {
-            int updatedRow = jdbcTemplate.executeUpdate("CREATE TABLE TEST_LEE(LEE_NAME VARCHAR)");
-            System.out.println("create table return value ? " + updatedRow);
-            String[] mynameArr = new String[]{
-                    "xxxx",
-                    "yyy",
-                    "zzz"
-            };
-            String sql = "INSERT INTO TEST_LEE(LEE_NAME) VALUES(?)";
-            int insertRowCount = jdbcTemplate.batchInsert(sql, mynameArr.length, (index, statement) -> {
-                statement.setString(1, mynameArr[index]);
-            });
-            System.out.println("batch insert row: " + insertRowCount);
-            String[] mynameArr1 = new String[]{
-                    "0xxxx",
-                    "0yyy",
-                    "0zzz"
-            };
-            jdbcTemplate.batchUpdate(new String[] {
-                    sql, sql, sql
-            }, (index, statement) -> {
-                statement.setString(1, mynameArr1[index]);
-            });
-        }
-        tableExists = jdbcTemplate.isTableExists("TEST_LEE");
-        System.out.println("table TEST_LEE exists ? " + tableExists);
-        List<Map<String, Object>> rowList = jdbcTemplate.queryForList("SELECT * FROM TEST_LEE");
-        logger.info("rowList: {}", rowList);
-        jdbcTemplate.getConnectionPool().close();
-        assertEquals(6, rowList.size());
     }
 
 }
